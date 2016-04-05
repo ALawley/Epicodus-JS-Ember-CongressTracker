a2639eff1ee8134aba6b1ba0da1e373aae555656
Anna Lawley <alawley1@gmail.com>
2016-04-05 09:03:27 -0700

    build legislator look up by zip code

---
+++ app/routes/results.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+
+export default Ember.Route.extend({
+ model: function(params) {
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=7019ff640bd14d7694d90361d2a936ac&zip=' + params.zip;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
+  }
+});
