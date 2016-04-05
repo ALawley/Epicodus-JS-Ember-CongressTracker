7f7138097607b9fb2eb914e5ce362950bdd662bb
Anna Lawley <alawley1@gmail.com>
2016-04-05 10:51:17 -0700

    add list of all committees

---
+++ app/routes/committees.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+
+export default Ember.Route.extend({
+ model: function() {
+   var url = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&apikey=7019ff640bd14d7694d90361d2a936ac'
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     console.log(responseJSON.results);
+     return responseJSON.results;
+   });
+  }
+});
--- app/routes/results.js
+++ app/routes/results.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
-
 export default Ember.Route.extend({
  model: function(params) {
    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=7019ff640bd14d7694d90361d2a936ac&zip=' + params.zip;
